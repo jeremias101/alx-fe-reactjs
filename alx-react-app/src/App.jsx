7cd57ffa899b8071464c0fca0484c0bf606dbb55
Jeremias <jeremiasaritigo@gmail.com>
2025-08-31 21:22:21 +0200

    Add files via upload

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,12 @@
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
 
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import './App.css';
+
+//this is for the fisrt react thing
 function App() {
   return (
     <div className="App">
@@ -9,4 +15,18 @@ function App() {
   )
 }
 
+//second react thing
 export default App
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
