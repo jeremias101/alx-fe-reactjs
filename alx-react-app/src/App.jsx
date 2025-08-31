288bdf126073b8ee8bfd18eb0be53db63a45f724
Jeremias <jeremiasaritigo@gmail.com>
2025-08-31 21:41:15 +0200

    Add files via upload

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,6 +5,8 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import './App.css';
+import UserProfile from './components/UserProfile';
+
 
 //this is for the fisrt react thing
 function App() {
@@ -30,3 +32,22 @@ function App() {
 
 export default App;
 
+function App() {
+  return (
+    <>
+      <h1>Welcome to My App</h1>
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+      <UserProfile 
+        name="Jeremias" 
+        age="22" 
+        bio="Learning React at ALX 🚀" 
+      />
+    </>
+  );
+}
+
+export default App;
