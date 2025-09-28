bb2bd2f619f1b1d2018f139d616c66fd0fcc7052
Jeremias <jeremiasaritigo@gmail.com>
2025-09-28 22:16:19 +0200

    Add files via upload

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -13,3 +13,17 @@ export default function App() {
     </div>
   );
 }
+
+
+import React from 'react';
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <div>
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
