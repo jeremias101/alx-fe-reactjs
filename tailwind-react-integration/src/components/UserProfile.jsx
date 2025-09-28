271dae16fdc3a8c40d0c132d94c343603665b3a2
Jeremias <jeremiasaritigo@gmail.com>
2025-09-28 22:44:57 +0200

    Add files via upload

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto my-8 md:my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto my-8 md:my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full mx-auto block sm:w-24 sm:h-24 md:w-36 md:h-36 object-cover"
+        className="rounded-full mx-auto block sm:w-24 sm:h-24 md:w-36 md:h-36 object-cover hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="text-lg md:text-xl text-blue-800 my-4 text-center font-semibold">
+      <h1 className="text-lg md:text-xl text-blue-800 my-4 text-center font-semibold hover:text-blue-500 transition-colors duration-300 ease-in-out">
         John Doe
       </h1>
       <p className="text-sm md:text-base text-gray-600 text-center">
