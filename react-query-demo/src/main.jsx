b1d650c5a25498f42e07f5e0050464ad03572688
Jeremias <jeremiasaritigo@gmail.com>
2025-10-19 21:54:11 +0200

    Add files via upload

--- react-query-demo/src/main.jsx
+++ react-query-demo/src/main.jsx
@@ -22,3 +22,15 @@ createRoot(document.getElementById("root")).render(
     </QueryClientProvider>
   </React.StrictMode>
 );
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
